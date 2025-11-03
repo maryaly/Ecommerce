3cd550636716390ed5de015dc3a7f897fe775508
Maryam Aliyari <maryamaliyari20965@yahoo.com>
2025-11-03 10:31:03 -0800

    Add - media query was added in product page

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -24,18 +24,21 @@ const ProductDisplay = (props) => {
             </div>
             <div className="productdisplay-right">
                 <h1>{product.name}</h1>
-                <div className="productdisplay-right-stars">
-                    <img src={star_icon} alt="" />
-                    <img src={star_icon} alt="" />
-                    <img src={star_icon} alt="" />
-                    <img src={star_icon} alt="" />
-                    <img src={star_dull_icon} alt="" />
-                    <p>(122)</p>
+                <div className="productdisplay-prices-rate">
+                    <div className="productdisplay-right-stars">
+                        <img src={star_icon} alt="" />
+                        <img src={star_icon} alt="" />
+                        <img src={star_icon} alt="" />
+                        <img src={star_icon} alt="" />
+                        <img src={star_dull_icon} alt="" />
+                        <p>(122)</p>
+                    </div>
+                    <div className="productdisplay-right-prices">
+                        <div className="productdisplay-right-price-old">${product.old_price}</div>
+                        <div className="productdisplay-right-price-new">${product.new_price}</div>
+                    </div>
                 </div>
-                <div className="productdisplay-right-prices">
-                    <div className="productdisplay-right-price-old">${product.old_price}</div>
-                    <div className="productdisplay-right-price-new">${product.new_price}</div>
-                </div>
+
                 <div className="productdisplay-right-description">
                     Stay cool and confident in this soft, breathable cotton t-shirt. Designed for everyday comfort, it features a relaxed fit and durable stitching that keeps its shape wash after wash. Perfect for pairing with jeans, shorts, or joggers for a laid-back, stylish look.
                 </div>
@@ -49,7 +52,7 @@ const ProductDisplay = (props) => {
                         <div>XXL</div>
                     </div>
                 </div>
-                <button onClick={()=>{addToCart(product.id)}}>ADD TO CART</button>
+                <button onClick={() => { addToCart(product.id) }}>ADD TO CART</button>
                 <p className='productdisplay-right-category'><span>Category :</span>Women , T-Shirt, Crop Top</p>
                 <p className='productdisplay-right-category'><span>Tags :</span>Modern , Latest</p>
             </div>
